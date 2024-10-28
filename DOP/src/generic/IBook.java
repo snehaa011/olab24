ee5b781b0366a09be08922fb238a11dc38844bcd
Sneha <snehakurupofficial@gmail.com>
2024-10-29 00:13:01 +0530

    bonus 1 & 2

---
+++ DOP/src/generic/IBook.java
@@ -0,0 +1,25 @@
+package generic;
+
+import newshelf.FictionType;
+
+public interface IBook<T> extends Comparable<T>{
+
+}
+
+record Comic(String title, int ageOfMainCharacter) implements IBook<Comic>{
+    public int compareTo(Comic a){
+        return this.title.compareTo(a.title);
+    }
+};
+
+record Fiction(String name, FictionType type) implements IBook<Fiction>{
+    public int compareTo(Fiction b){
+        return b.hashCode()-this.hashCode();
+    }
+};
+
+record TextBook(String subject) implements IBook<TextBook>{
+    public int compareTo(TextBook b){
+        return b.subject.compareTo(this.subject);
+    }
+};
